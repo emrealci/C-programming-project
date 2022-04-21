a10ee103fd4ccb9eb188e40c259743ccac1154cb
Emre Alçi <87760380+WualFabre@users.noreply.github.com>
2022-04-21 17:49:52 +0300

    Update project2_solution.c

--- C_project 2/project2_solution.c
+++ C_project 2/project2_solution.c
@@ -5,7 +5,7 @@ int main()
 {
     int number;
     scanf("%d",&number);
-      if (number<0){
+    if (number<0){
         printf("Please enter an upper bound: %d\n",number);
         printf("Upper bound can not be negative.");
     }
@@ -15,84 +15,59 @@ int main()
         printf("There are 0 ultra magic numbers up to %d. ",number);
     }
     else{
-	printf("Please enter an upper bound: %d\n",number);
-    int m=100;
-     int value=0;
-while(m<number) {
+        printf("Please enter an upper bound: %d\n",number);
+        int m=100;
+        int value=0;
+        while(m<number) {
 	
-    int count=-1;
-    int j;
-    	int sayac=0;
-      int buffer,buffer1,buffer2;
+            int count=-1;
+            int j;
+    	    int sayac=0;
+            int buffer,buffer1,buffer2;
       
-      buffer=m;
-      buffer1=m;
-      buffer2=m;
+           buffer=m;
+           buffer1=m;
+           buffer2=m;
 
-   
-    while(buffer>0){
+           while(buffer>0){
     	
-      buffer=buffer/10;
-     count=count+1;
-      
-    }
+              buffer=buffer/10;
+              count=count+1;
+            }
      
-     int arr[count];
-     int arr2[count];
+          int arr[count];
+          int arr2[count];
      
-    while(buffer1>0){
-    	
-       buffer2=buffer1%10;
-    	arr[sayac]=buffer2;
-    	sayac=sayac+1;
-       buffer1=buffer1/10;
-     
-      
-    }
-   
-    
-    
-     for(j=0;j<=count;j++){
-     	
-        arr2[j]=arr[count-j];
+         while(buffer1>0){
+    	    buffer2=buffer1%10;
+    	    arr[sayac]=buffer2;
+    	    sayac=sayac+1;
+           buffer1=buffer1/10;
+         }
+        for(j=0;j<=count;j++){
+            arr2[j]=arr[count-j];
     }
     
-     	
+        int A=0;
+        int B;
 		 
-	
-		 	
-		 int A=0;
-		 int B;
-		 
-		 for(B=0;B<=count;B++){
-		 	
-		 	 if( arr2[B]==arr[B]){
-		 	 	
-		 	 	
-			  
-		 	A=A+1;
-		 	
-		 }
+        for(B=0;B<=count;B++){
+            if( arr2[B]==arr[B]){
+	 	A=A+1;
+	     }
 	}
 		 
-		 if( A==(count+1) && arr[0] != arr[1]) {
-		 	
-		 	printf("%d ",m);
-		 	value=value+1;
-		 	if(value%9==0){
-		 		
-		 		printf("\n");
-			 }
-		 }
-		 		
-		 	
-			
-	
-	
+       if( A==(count+1) && arr[0] != arr[1]) {
+	    printf("%d ",m);
+	    value=value+1;
+	    if(value%9==0){
+	    printf("\n");
+	    }
+        }		 						
 	m++;
 
 }
-printf("\n");
-printf("There are %d ultra magic numbers up to %d.",value,number);
+    printf("\n");
+    printf("There are %d ultra magic numbers up to %d.",value,number);
 }
 }
